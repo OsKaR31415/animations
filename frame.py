b184857c1793590969fa36b62108f7590e6046a2
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-05-12 05:25:12 +0200

    Create the structure for the Frame object

--- frame.py
+++ frame.py
@@ -1,10 +1,16 @@
+from typing import TypeVar
 
+# Type to represent a frame modification
+# That is a method of *Frame* that modifies the frame
+FrameModification = TypeVar('FrameModification')
 
 class Frame:
     def __init__(self, scr) -> None:
+        # scr is a curses stdscr
         self.scr = scr
 
     def put_text(self, y: int, x: int, text: str) -> None:
+        pass
 
 
 
