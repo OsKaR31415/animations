337a7dc95a5df33c021b8459ae6a20e2626a6865
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-05-16 00:21:43 +0200

    better frame timing (should be constant for almost any animations)

--- anim_player.py
+++ anim_player.py
@@ -1,11 +1,16 @@
-from time import sleep
+from time import time
+
+FRAME_DELAY = 0.02
 
 def play(frame, anim):
     for modif_list in anim:
+        time_before_modifs = time()
         for modif in modif_list:
             modif(frame)
         # refresh only after all the modifications
         frame.refresh()
-        # sleep(0.02)
+        # wait as long as needed so the frame is here for *FRAME_DELAY*
+        while time() < FRAME_DELAY + time_before_modifs:
+            pass
     frame.pause()
 
