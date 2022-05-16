8ffd5de87fa5fd14a119432db5346f3532c0b823
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-05-17 01:22:52 +0200

    more resilient animation player

--- anim_player.py
+++ anim_player.py
@@ -1,8 +1,16 @@
 from time import time
+from animations import Anim
 
 FRAME_DELAY = 0.02
 
-def play(frame, anim):
+
+def play(frame, animation):
+    # ensure that animation is an *Anim* object
+    if isinstance(animation, Anim):
+        anim = animation
+    else:
+        anim = Anim(frame, animation)
+    # the animation loop
     for modif_list in anim:
         time_before_modifs = time()
         for modif in modif_list:
