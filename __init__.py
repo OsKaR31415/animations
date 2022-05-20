a08bbd7a76b663d1929b8cce57bc8b6c26d146c3
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-05-20 02:06:31 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -23,24 +23,24 @@ def screen_saver(fr):
     dot2 = Anim(fr, fadeinout(10, 11, "•"), after=10)
     dot3 = Anim(fr, fadeinout(10, 12, "•"), after=20)
     dots = dot1 >> dot2 >> dot3
-    anim = Wait(fr, dots, 3000)
-    play(fr, Anim(frame, anim))
+    play(fr, Anim(fr, dots))
 
 def main(scr):
     initialize_curses_colors()
     fr = Frame(scr)
-    screen_saver(fr)
+    # screen_saver(fr)
 
     # fade_in_out = Anim(fr, fadein(5, 5, "coucou")) > Anim(fr, fadeout(5, 5, "coucou"))
 
     coucou = Anim(fr, fadein(5, 5, "coucou")) >> Anim(fr, fadein(6, 5, "c'est moi"))
+    coucou2 = Anim(fr, fadein(5, 5, "coucou")) >> Anim(fr, fadein(6, 5, "c'est moi"))
     hdyd = Anim(fr, fadein(10, 10, "how do you do ?"))
     well = Anim(fr, fadein(20, 5, "je vais well !"))
-    anim = coucou & hdyd & well
+    anim = coucou & Anim(fr, coucou)
 
 
     # anim = Anim(fr, addstr(5, 5, "test", curses.color_pair(1)))
-    # play(fr, anim)
+    play(fr, anim)
 
 
 
